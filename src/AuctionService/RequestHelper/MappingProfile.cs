91e973e875d51e5bc829ef112378273996d7ae72
ravikantpal1408 <ravikantpal123@gmail.com>
2024-06-17 12:53:25 +0530

    production fix given

--- src/AuctionService/RequestHelper/MappingProfile.cs
+++ src/AuctionService/RequestHelper/MappingProfile.cs
@@ -16,5 +16,6 @@ public class MappingProfile : Profile
         CreateMap<CreateAuctionDto, Item>();
         CreateMap<CreateAuctionDto, AuctionCreated>();
         // this the work i was tend to do 
+        // production fix i have given 
     }
 }
