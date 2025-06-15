f453d19f90cc7adb93edf8da4e771092d9bc1515
ravikantpal1408 <ravikantpal123@gmail.com>
2025-06-15 21:02:00 +0530

    section 8.1

---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './globals.css';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-4xl font-semibold'>Welcome to the Web App!</h1>
+    </div>
+  );
+}
