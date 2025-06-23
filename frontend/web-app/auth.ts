da08b758e95fe45fad05cf936eec4c043a647f1e
ravikantpal1408 <ravikantpal123@gmail.com>
2025-06-23 23:21:58 +0530

    section 9.109 done

---
+++ frontend/web-app/auth.ts
@@ -0,0 +1,18 @@
+import NextAuth, { Profile } from "next-auth";
+import { OIDCConfig } from "next-auth/providers";
+import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6";
+
+export const {
+    handlers, signIn, signOut, auth
+} = NextAuth({
+    providers: [
+        DuendeIDS6Provider({
+            id: "id-server",
+            clientId: "nextApp",
+            clientSecret: "secret",
+            issuer: "http://localhost:5001",
+            authorization: { params: { scope: "openid profile auctionApp" } },
+            idToken: true,
+        } as OIDCConfig<Profile>),
+    ]
+})
