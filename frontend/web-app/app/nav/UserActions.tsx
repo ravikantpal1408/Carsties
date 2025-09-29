e46e9411d81a1fa1772ec09c5cc9032a88a1be68
ravikantpal <ravikantpal@ravikantpals-MacBook-Pro.local>
2025-09-30 01:08:56 +0530

    made changes

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,39 +1,34 @@
-'use client'
-import { Dropdown, DropdownDivider, DropdownItem } from 'flowbite-react'
-import { User } from 'next-auth'
-import { signOut } from 'next-auth/react'
-import Link from 'next/link'
-import React from 'react'
-import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
-import { HiCog, HiUser } from 'react-icons/hi'
+"use client";
+import { Dropdown, DropdownDivider, DropdownItem } from "flowbite-react";
+import { User } from "next-auth";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from "react-icons/ai";
+import { HiCog, HiUser } from "react-icons/hi";
 
 type Props = {
-    user: User
-}
+  user: User;
+};
 
 export default function UserActions({ user }: Props) {
-    return (
-        <Dropdown inline label={`Welcome ${user.name}`} className='cursor-pointer'>
-            <DropdownItem icon={HiUser}>
-                My Auctions
-            </DropdownItem>
-            <DropdownItem icon={AiFillTrophy}>
-                Auction Won
-            </DropdownItem>
-            <DropdownItem icon={AiFillCar}>
-                <Link href={'/auctions/create'}>
-                    Sell My Car
-                </Link>
-            </DropdownItem>
-            <DropdownItem icon={HiCog}>
-                <Link href='/session'>
-                    Session (dev only)!
-                </Link>
-            </DropdownItem>
-            <DropdownDivider />
-            <DropdownItem icon={AiOutlineLogout} onClick={() => signOut({ redirectTo: '/' })}>
-                Sign Out
-            </DropdownItem>
-        </Dropdown>
-    )
+  return (
+    <Dropdown inline label={`Welcome ${user.name}`} className="cursor-pointer">
+      <DropdownItem icon={HiUser}>My Auctions</DropdownItem>
+      <DropdownItem icon={AiFillTrophy}>Auction Won</DropdownItem>
+      <Link href={"/auctions/create"}>
+        <DropdownItem icon={AiFillCar}>Sell My Car</DropdownItem>
+      </Link>
+      <Link href="/session">
+        <DropdownItem icon={HiCog}>Session (dev only)!</DropdownItem>
+      </Link>
+      <DropdownDivider />
+      <DropdownItem
+        icon={AiOutlineLogout}
+        onClick={() => signOut({ redirectTo: "/" })}
+      >
+        Sign Out
+      </DropdownItem>
+    </Dropdown>
+  );
 }
